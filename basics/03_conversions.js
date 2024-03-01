6c5d1837b7b97dc373fc8a99d17ab2180ade57e8
Garv Chauhan <133596325+chauhangarv@users.noreply.github.com>
2024-03-01 18:17:43 +0000

    conversion

--- basics/03_conversions.js
+++ basics/03_conversions.js
@@ -16,7 +16,7 @@ let valueInNumber = Number(score);
 let isLoggedIn = "h";
 
 let booleanIsLoggedIn = Boolean(isLoggedIn);
-console.log(booleanIsLoggedIn);
+// console.log(booleanIsLoggedIn);
 
 // 1 => true; 0 => false;
 // "" => false;
@@ -25,6 +25,43 @@ console.log(booleanIsLoggedIn);
 let someName = null;
 
 let string = String(someName);
-console.log(string);
+// console.log(string);
 
 // string => everything is converted
+
+
+let value = 3;
+let negValue = -value;
+// console.log(negValue);
+
+// console.log(2+2); // addition
+// console.log(2-2); // sub
+// console.log(2*2); // multiplication
+// console.log(2**2);  // power
+// console.log(2/2);  // division
+// console.log(2%2);  // reminder
+
+let str1 = "Hello";
+let str2 = " World";
+let str3 = str1 + str2;
+
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + "2");
+// console.log(1 + 2);
+
+// console.log(1 + 2 + "2");
+// console.log("1" + 2 + 2);
+
+/* 
+agr string first m h to sbhi to string ki trha hi treat kra jayega...
+*/
+
+// console.log(+"");
+
+let game = 100;
+++game;   // prefixoperator
+game++;    // postfixoperator
+// console.log(game);
