b1b754a9809f9f0631e593cb362005a561656ffd
Garv Chauhan <133596325+chauhangarv@users.noreply.github.com>
2024-03-01 07:57:37 +0000

    datatypes

---
+++ basics/02_basics.js
@@ -0,0 +1,29 @@
+"use strict"; // treat all JS code as newer version
+
+// alert(3 + 3); // we are using nodejs, not browser
+
+// console.log(3 - 3);
+// console.log("Hello");  // code should be readable
+
+let name = "hello";
+let age = 18;
+let isLoggedIn = false;
+let state;
+
+console.table([name, age, isLoggedIn, state]);
+
+/*
+Primitive Data Types
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => value is not assigned
+*/
+
+/*
+Object
+*/
+console.log(typeof null);  // object
+console.log(typeof undefined);  // undefined
